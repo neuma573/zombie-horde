e278a24220a3d196be3c9c76509ef186677e248d
neuma573 <crye@kakao.com>
2026-08-05 11:25:24 +0900

    feat: improve intersection road markings (#75)

--- src/effects/gameAssetPreloader.ts
+++ src/effects/gameAssetPreloader.ts
@@ -1,13 +1,19 @@
 import crosswalkUrl from '../assets/sidewalk.png';
+import noStoppingZoneUrl from '../assets/no_stopping_zone_transparent.svg';
+import roadDiamondOutlineUrl from '../assets/road_diamond_outline.png';
 import pistolIconUrl from '../assets/weapons/pistol.png';
 import rifleIconUrl from '../assets/weapons/rifle.png';
 import { CHARACTER_CLASS_OPTIONS } from '../config/menuConfig';
 import { WEAPON_AUDIO_ASSETS } from '../config/weaponAudioConfig';
 
 export const CROSSWALK_TEXTURE_KEY = 'crosswalk';
+export const NO_STOPPING_ZONE_TEXTURE_KEY = 'no-stopping-zone';
+export const ROAD_DIAMOND_OUTLINE_TEXTURE_KEY = 'road-diamond-outline';
 
 export const GAME_IMAGE_ASSETS = {
   [CROSSWALK_TEXTURE_KEY]: crosswalkUrl,
+  [NO_STOPPING_ZONE_TEXTURE_KEY]: noStoppingZoneUrl,
+  [ROAD_DIAMOND_OUTLINE_TEXTURE_KEY]: roadDiamondOutlineUrl,
   'weapon-pistol': pistolIconUrl,
   'weapon-rifle': rifleIconUrl,
 } as const;
